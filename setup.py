76b80ccfdd3c508c106a541298f810991c2ee17c
rich_text_diff <liukaiash@gmail.com>
2018-07-09 14:25:08 +0800

    auto to_unicode

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name='rich_text_diff',
-    version='0.0.1',
+    version='0.0.2',
     author='liukai',
     author_email='liukai@zhihu.com',
-    description='diff rich text',
-    packages=['diff'],
+    description='rich_text_diff rich text',
+    packages=['rich_text_diff'],
     test_suite='nose.collector',
-    url='https://github.com/c1ay/diff',
+    url='https://github.com/c1ay/rich_text_diff',
     tests_require=['nose'],
     install_requires=[
         'lxml',
