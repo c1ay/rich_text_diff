0b730d102657b206bb6287e5d2fc995130a59be5
rich_text_diff <liukaiash@gmail.com>
2018-07-10 18:23:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(
     name='rich_text_diff',
-    version='0.0.2',
+    version='0.0.3',
     author='liukai',
     author_email='liukai@zhihu.com',
-    description='rich_text_diff rich text',
+    description='support rich text diff',
     packages=['rich_text_diff'],
     test_suite='nose.collector',
     url='https://github.com/c1ay/rich_text_diff',
