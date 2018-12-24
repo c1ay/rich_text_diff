5aebfaec73d26508354ed386fec3945e5caa66e3
c1ay <liukaiash@gmail.com>
2018-12-24 16:10:05 +0800

    fix: setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
 setup(
     name='rich_text_diff',
     version='0.0.6',
@@ -9,8 +13,8 @@ setup(
     description='support rich text diff',
     packages=['rich_text_diff'],
     test_suite='nose.collector',
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
-    long_description='readme',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    long_description=readme,
     long_description_content_type='text/markdown',
     url='https://github.com/c1ay/rich_text_diff',
     tests_require=['nose'],
